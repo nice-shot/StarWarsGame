751793ba39db93106ddc99e94271b00e92b57b4e
nice-shot <shefferu@gmail.com>
2017-03-11 23:17:14 +0200

    added algorythem for efficient shilding in Kamikaze brain

--- Assets/Scripts/starwars/brains/KamikazeBrain.cs
+++ Assets/Scripts/starwars/brains/KamikazeBrain.cs
@@ -20,11 +20,49 @@ public class KamikazeBrain : SpaceshipBrain {
 		}
 	}
 
+	private const float DANGER_RADIUS = 3f;
+
+	private bool ShouldRaiseShield () {
+		var position = spaceship.Position;
+		foreach (var shot in Space.Shots) {
+			// Ignore non-existing shots
+			if (!shot.IsAlive) continue;
+			var shotRelPos = spaceship.ClosestRelativePosition (shot);
+			if (Vector2.Distance (position, shot.Position) < DANGER_RADIUS) {
+				Debug.Log ("Shot is close");
+				return true;
+			}
+		}
+
+		foreach (var ship in Space.Spaceships) {
+			if (spaceship == ship || !ship.IsAlive)	continue;
+			var shipRelPos = spaceship.ClosestRelativePosition (ship);
+			var distance = Vector2.Distance (position, ship.Position);
+//			Debug.Log (ship.Name + " is at: " + ship.Position);
+//			Debug.Log ("Relative position is: " + shipRelPos);
+//			Debug.Log ("I'm at: " + position);
+//			Debug.Log ("Simple distance is: " + distance);
+//			Debug.Log ("Short distnace is: " + Vector2.Distance (position, shipRelPos));
+			if (distance < DANGER_RADIUS) {
+				Debug.Log (ship.Name + " is close");
+				return true;
+			}
+		}
+		return false;
+	}
 	// <summary>
 	// Tries to ram into other ships while using the shield
 	// </summary>
 	public override Action NextAction () {
-		return DoNothing.action;	
+		if (ShouldRaiseShield ()) {
+			if (spaceship.CanRaiseShield) {
+				return ShieldUp.action;
+			}
+		} else if (spaceship.IsShieldUp) {
+			return ShieldDown.action;
+		}
+
+		return TurnLeft.action;
 	}
 }
 }
