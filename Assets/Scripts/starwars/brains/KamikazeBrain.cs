53e1a2b93e9fdb788c1ae6631e1525a8b9a907d9
nice-shot <shefferu@gmail.com>
2017-03-12 00:02:42 +0200

    Added smarter tracking for Kamikaze

--- Assets/Scripts/starwars/brains/KamikazeBrain.cs
+++ Assets/Scripts/starwars/brains/KamikazeBrain.cs
@@ -9,60 +9,88 @@ public class KamikazeBrain : SpaceshipBrain {
 			return "Kamikaze";
 		}
 	}
+
 	public override Color PrimaryColor {
 		get {
 			return Color.yellow;
 		}
 	}
+
 	public override SpaceshipBody.Type BodyType {
 		get {
 			return SpaceshipBody.Type.XWing;
 		}
 	}
 
-	private const float DANGER_RADIUS = 3f;
+	private const float DANGER_RADIUS = 4f;
+	private Spaceship target = null;
 
-	private bool ShouldRaiseShield () {
-		var position = spaceship.Position;
-		foreach (var shot in Space.Shots) {
-			// Ignore non-existing shots
-			if (!shot.IsAlive) continue;
-			var shotRelPos = spaceship.ClosestRelativePosition (shot);
-			if (Vector2.Distance (position, shot.Position) < DANGER_RADIUS) {
-				Debug.Log ("Shot is close");
-				return true;
-			}
-		}
-
-		foreach (var ship in Space.Spaceships) {
-			if (spaceship == ship || !ship.IsAlive)	continue;
-			var shipRelPos = spaceship.ClosestRelativePosition (ship);
-			var distance = Vector2.Distance (position, ship.Position);
-//			Debug.Log (ship.Name + " is at: " + ship.Position);
-//			Debug.Log ("Relative position is: " + shipRelPos);
-//			Debug.Log ("I'm at: " + position);
-//			Debug.Log ("Simple distance is: " + distance);
-//			Debug.Log ("Short distnace is: " + Vector2.Distance (position, shipRelPos));
-			if (distance < DANGER_RADIUS) {
-				Debug.Log (ship.Name + " is close");
-				return true;
-			}
-		}
-		return false;
-	}
 	// <summary>
 	// Tries to ram into other ships while using the shield
 	// </summary>
-	public override Action NextAction () {
-		if (ShouldRaiseShield ()) {
+	public override Action NextAction ()
+	{
+		if (ShouldRaiseShield()) {
 			if (spaceship.CanRaiseShield) {
 				return ShieldUp.action;
 			}
 		} else if (spaceship.IsShieldUp) {
 			return ShieldDown.action;
 		}
+			
 
-		return TurnLeft.action;
+		var position = spaceship.Position;
+
+		// Target closest ship
+		foreach (var ship in Space.Spaceships) {
+			if (spaceship == ship || target == ship || !ship.IsAlive) continue;
+			if (target != null) { 
+				var currentDistance = Vector2.Distance(position, target.Position);
+				var shipDistance = Vector2.Distance(position, ship.Position);
+				if (currentDistance > shipDistance) {
+					target = ship;
+				}
+
+			} else {
+				target = ship;
+			}
+		}
+
+		if (target != null) {
+			// Hunt (totally copied hunter brain...)
+			var pos = spaceship.ClosestRelativePosition(target);
+			var forwardVector = spaceship.Forward;
+			var angle = pos.GetAngle(forwardVector);
+			if (angle >= 10) return TurnLeft.action;
+			if (angle <= -10) return TurnRight.action;
+		}
+
+		return DoNothing.action;
+	}
+
+	// Raises shiled whenever a shot or spaceship is nearby
+	private bool ShouldRaiseShield ()
+	{
+		var position = spaceship.Position;
+
+		foreach (var shot in Space.Shots) {
+			// Ignore non-existing shots
+			if (!shot.IsAlive) continue;
+			if (Vector2.Distance(position, shot.Position) < DANGER_RADIUS) {
+				Debug.Log("Shot is close");
+				return true;
+			}
+		}
+
+		foreach (var ship in Space.Spaceships) {
+			// Ignore self and dead ships
+			if (spaceship == ship || !ship.IsAlive) continue;
+			if (Vector2.Distance(position, ship.Position) < DANGER_RADIUS) {
+				Debug.Log(ship.Name + " is close");
+				return true;
+			}
+		}
+		return false;
 	}
 }
 }
